d2fdf94c9a6f9b338cb7e47a2023d22cb1f2d489
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-29 21:31:36 -0300

    Cores personalizadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <div className="text-blue-500 bg-green-500 border-2 border-white border-b-red-400">Olá, Mundo!</div>
+      <div className="text-blue-500 bg-green-500 border-2 border-white border-b-red-400 bg-blackRed">Olá, Mundo!</div>
     </div>
   )
 }
