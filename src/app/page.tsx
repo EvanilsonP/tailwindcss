7389712b99487974d8adc365d59ea102b62450ae
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-30 19:36:39 -0300

    Containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,31 +101,39 @@
 //     </div>
 //   )
 // }
+// exercicio tela login
+// export default function Page() {
+//     return (
+//         <div className="w-screen h-screen p-5 bg-gray-900">
+
+//             <div className="mt-5">
+//                 <h1 className="text-3xl font-boldtext-center">Sistema de Login</h1>
+
+//                 <div className="my-6">
+//                     <label className="block text-lg mb-2" htmlFor="emailField">Endereço de email</label>
+//                     <input className="w-full p-2 rounded-md text-lg bg-gray-800 border border-gray-600 outline-yellow-500" type="email" name="" id="emailField" />
+//                 </div>
+
+//                 <div className="my-6">
+//                     <label className="block text-lg mb-2" htmlFor="passwordField">Senha</label>
+//                     <input className="w-full p-2 rounded-md text-lg bg-gray-800 border border-gray-600 outline-yellow-500" type="password" name="" id="passwordField" />
+//                 </div>
+
+//                 <div className="my-6">
+//                     <button className="w-full p-2 rounded-md text-lg font-bold border-none text-white bg-yellow-500">Entrar</button>
+//                 </div>
+//             </div>
+//             <div className="text-center text-gray-400">
+//                 Não é membro? <a className="text-yellow-500 font-bold" href="">Faça seu cadastro agora.</a>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default function Page() {
     return (
-        <div className="w-screen h-screen p-5 bg-gray-900">
-
-            <div className="mt-5">
-                <h1 className="text-3xl font-boldtext-center">Sistema de Login</h1>
-
-                <div className="my-6">
-                    <label className="block text-lg mb-2" htmlFor="emailField">Endereço de email</label>
-                    <input className="w-full p-2 rounded-md text-lg bg-gray-800 border border-gray-600 outline-yellow-500" type="email" name="" id="emailField" />
-                </div>
-
-                <div className="my-6">
-                    <label className="block text-lg mb-2" htmlFor="passwordField">Senha</label>
-                    <input className="w-full p-2 rounded-md text-lg bg-gray-800 border border-gray-600 outline-yellow-500" type="password" name="" id="passwordField" />
-                </div>
-
-                <div className="my-6">
-                    <button className="w-full p-2 rounded-md text-lg font-bold border-none text-white bg-yellow-500">Entrar</button>
-                </div>
-            </div>
-            <div className="text-center text-gray-400">
-                Não é membro? <a className="text-yellow-500 font-bold" href="">Faça seu cadastro agora.</a>
-            </div>
+        <div className="container bg-gray-800 mx-auto">
+            IO
         </div>
     )
 }
